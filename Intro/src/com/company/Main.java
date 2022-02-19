945b689eb79fe39803beb78860fbbb766bddc442
BorisBel02 <borikbeli@gmail.com>
2022-02-19 12:08:58 +0700

    first lab / all

---
+++ Intro/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Map;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    if(args.length != 2){
+            System.out.println("bad input");
+            System.exit(0);
+        }
+    }
+    Map<StringBuilder, Integer> map;
+    ReadFile a = new ReadFile();
+}
